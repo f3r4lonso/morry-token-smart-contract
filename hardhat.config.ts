a81f2033f0a58e881fe9675ebf12609c4a3eae29
f3r4lonso <alonso.fernando.morry@gmail.com>
2026-04-11 21:11:32 +0200

    das

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,12 @@
-import "@nomicfoundation/hardhat-toolbox";
+ import "@nomicfoundation/hardhat-toolbox";
+require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: [process.env.SEPOLIA_PRIVATE_KEY],
+    },
+  },
 };
