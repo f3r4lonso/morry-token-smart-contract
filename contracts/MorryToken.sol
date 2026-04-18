34674712f2d10e0fe07e7ad16e0a83482cd15552
f3r4lonso <alonso.fernando.morry@gmail.com>
2026-04-18 14:41:59 +0200

    .

--- contracts/MorryToken.sol
+++ contracts/MorryToken.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.28;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -9,16 +9,20 @@ import "@openzeppelin/contracts/access/Ownable.sol";
  * @dev ERC-20 token con mint controlado y burn.
  */
 contract MorryToken is ERC20, Ownable {
+    uint256 public immutable MAX_SUPPLY;
 
-    constructor(uint256 initialSupply)
+    constructor(uint256 maxSupply)
         ERC20("Morry Token", "MORRY")
         Ownable(msg.sender)
     {
-        _mint(msg.sender, initialSupply * 10 ** decimals());
+        require(maxSupply > 0, "Invalid max supply");
+        MAX_SUPPLY = maxSupply;
+        _mint(msg.sender, maxSupply);
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount * 10 ** decimals());
+        require(totalSupply() + amount <= MAX_SUPPLY, "Max supply exceeded");
+        _mint(to, amount);
     }
 
     function burn(uint256 amount) public {
